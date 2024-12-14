66da6258197569c74d88b79d073c9391b9976341
Lara-Capila <laracapilak@gmail.com>
2024-12-14 20:13:27 -0300

    refactor(Hero): Refactor Hero component to v2 styles

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,87 +1,132 @@
 "use client";
 
-import { motion } from "framer-motion";
+import {
+  easeIn,
+  easeInOut,
+  motion,
+  useScroll,
+  useTransform,
+} from "framer-motion";
 import Image from "next/image";
-import Link from "next/link";
+import { useRef } from "react";
 import { useSectionInView } from "../hooks/useSectionInView";
-import GradientText from "./GradientText";
+
+const handWaveAnimation = {
+  rotate: [
+    "0deg",
+    "15deg",
+    "-10deg",
+    "15deg",
+    "-10deg",
+    "15deg",
+    "-10deg",
+    "15deg",
+    "-10deg",
+    "15deg",
+    "0deg",
+  ],
+  transition: {
+    duration: 1.5,
+    ease: easeInOut,
+  },
+};
+
+const animateIn1 = {
+  opacity: [0, 1],
+  y: ["16px", "0px"],
+  transition: {
+    delay: 1.5,
+    duration: 0.7,
+    ease: easeIn,
+  },
+};
+
+const animateIn2 = {
+  ...animateIn1,
+  transition: {
+    ...animateIn1.transition,
+    delay: 2,
+  },
+};
 
 const Hero = () => {
-	const { ref } = useSectionInView("Home", 0.5);
+  const { ref } = useSectionInView("Home", 0.5);
+  const imgRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: imgRef,
+  });
 
-	return (
-		<section
-			ref={ref}
-			id="home"
-			className="grid grid-cols-2 sm:flex sm:justify-between h-screen scroll-mt-[100rem]"
-		>
-			<motion.div
-				initial={{ opacity: 0, scale: 0 }}
-				animate={{ opacity: 1, scale: 1 }}
-				transition={{ duration: 0.5 }}
-				className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
-			>
-				<h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-tight font-extrabold">
-					<span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
-						Olá, sou
-					</span>
-					<br />
-					Lara Capila
-				</h1>
-				<p className="text-grayText text-base sm:text-lg lg:text-xl mb-6 max-w-3xl">
-					Sou desenvolvedora
-					<GradientText text="Front End" margin="ml-1" /> com
-					<GradientText text="2 anos de experiência" margin="ml-1 mr-1" />
-					com React (Next.js). Gosto de criar sites e aplicativos (React
-					Native).
-				</p>
-				<div className="flex flex-wrap gap-4">
-					<Link
-						href="#contact"
-						className="
-							px-6 py-3 rounded-full
-							w-full sm:w-fit
-							bg-gradient-to-br from-blue-500 via-primary-500
-							to-secondary-500 hover:bg-slate-200
-							text-white hover:scale-110
-						"
-					>
-						Me contrate
-					</Link>
-					<a
-						className="
-							p-1 rounded-full
-							w-full sm:w-fit
-							bg-gradient-to-br from-blue-500 via-primary-500 to-secondary-500
-							hover:bg-slate-800 text-white hover:scale-110 cursor-pointer
-						"
-						href="/Lara-Capila.pdf"
-						download
-					>
-						<span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-							Download CV
-						</span>
-					</a>
-				</div>
-			</motion.div>
-			<motion.div
-				initial={{ opacity: 0, scale: 0 }}
-				animate={{ opacity: 1, scale: 1 }}
-				transition={{ duration: 0.5 }}
-				className="col-span-4 place-self-center mt-4 lg:mt-0 hidden sm:block"
-			>
-				<div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[370px] lg:h-[370px] relative">
-					<Image
-						src="/images/me.png"
-						alt="an animation image of me"
-						className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-						width={350}
-						height={350}
-					/>
-				</div>
-			</motion.div>
-		</section>
-	);
+  const rotate = useTransform(scrollYProgress, [0, 1], ["0deg", "-15deg"]);
+
+  return (
+    <section
+      ref={ref}
+      className="pt-36 sm:pt-0 flex flex-col sm:flex-row h-dvh items-center gap-6 sm:justify-between"
+      id="home"
+    >
+      <div className="text sm:w-[60%]">
+        <motion.div
+          className="grid grid-cols-9 w-fit smm:flex gap-2 mb-2 xl:mb-6"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.1, ease: "easeOut" }}
+        >
+          <span className="text-white/60 text-xl smm:text-2xl mb-3 smm:mb-0 lg:text-3xl col-span-4">
+            Olá
+          </span>
+          <motion.div
+            animate={handWaveAnimation}
+            style={{ transformOrigin: "bottom right" }}
+            className="col-span-3"
+          >
+            <Image
+              src="/hand-wave.svg"
+              width={30}
+              height={30}
+              alt="hand-waving"
+            />
+          </motion.div>
+        </motion.div>
+        <motion.h1
+          className="text-[32px] smm:text-[40px] md:text-5xl lg:text-6xl xl:text-7xl leading-tight font-bold"
+          initial={{ opacity: 0 }}
+          animate={animateIn1}
+        >
+          <p className="text-white/60 inline">Sou </p>
+          <span className="bg-gradient-to-br bg-clip-text text-transparent from-[#7CC0C4] via-[#548FBA] to-[#3C84C7]">
+            Lara Capila
+          </span>
+          <p>Desenvolvedora Front End</p>
+        </motion.h1>
+
+        <motion.p
+          initial={{ opacity: 0 }}
+          animate={animateIn2}
+          className="text-white/40 text-xl smm:text-2xl lg:text-3xl xl:text-4xl mt-3 smm:mt-6 "
+        >
+          Focada em criar interfaces de usuário que conectam.
+        </motion.p>
+      </div>
+
+      <div>
+        <motion.div
+          ref={imgRef}
+          style={{ rotate }}
+          className="h-image flex items-center w-[310px] h-[380px] xl:w-[390px] xl:h-[470px] justify-center relative"
+          initial={{ opacity: 0 }}
+          animate={animateIn1}
+        >
+          <Image
+            src="/images/me.png"
+            priority
+            fill
+            alt="Lara's picture"
+            className="bg-image-radial px-10 pt-20"
+          />
+        </motion.div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
