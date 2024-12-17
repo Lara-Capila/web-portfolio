9c843cd9ab505de91cb90743e1759d548fd91631
Lara-Capila <laracapilak@gmail.com>
2024-12-17 18:32:05 -0300

    chore: Remove unnused package

--- src/components/contact-section/Contact.tsx
+++ src/components/contact-section/Contact.tsx
@@ -4,95 +4,10 @@ import { GithubLink } from "@components/GithubLink";
 import { LinkedinLink } from "@components/LinkedinLink";
 import Title from "@components/ui/Title";
 import { WhatsAppLink } from "@components/WhatsAppLink";
-import { useRef, useState } from "react";
-import { useForm } from "react-hook-form";
-import { toast } from "react-toastify";
 import { useSectionInView } from "../../hooks/useSectionInView";
 
-interface FormData {
-  userName?: string;
-  userEmail?: string;
-  userMessage?: string;
-}
-
-const defaultValues: FormData = {
-  userEmail: undefined,
-  userMessage: undefined,
-  userName: undefined,
-};
-
 const Contact = () => {
-  const [emailSubmitted, setEmailSubmitted] = useState(false);
-  const [loading, setLoading] = useState(false);
-
-  const {
-    formState: { errors },
-    register,
-    handleSubmit,
-    reset,
-    getValues,
-  } = useForm<FormData>({
-    defaultValues,
-    mode: "onBlur",
-  });
-
   const { ref } = useSectionInView("Contato");
-
-  const formRef = useRef<HTMLFormElement>(null);
-  console.log(getValues());
-
-  const onSubmit = async (data: FormData) => {
-    setLoading(true);
-
-    const bodyData = {
-      email: data.userEmail,
-      subject: `${data.userName} enviou uma mensagem através do portifólio`,
-      message: data.userMessage,
-    };
-
-    const JSONdata = JSON.stringify(bodyData);
-    const endpoint = "/api/send";
-
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSONdata,
-    };
-
-    const response = await fetch(endpoint, options);
-
-    if (!response.ok) {
-      toast.error(
-        "Ocorreu um erro ao enviar a mensagem. Peço que tente novamente ou entre em contato através dos outros canais.",
-        {
-          position: "bottom-right",
-        }
-      );
-      setLoading(false);
-      return;
-    }
-
-    if (response.status === 200) {
-      toast.success("Mensagem enviada com sucesso!", {
-        position: "bottom-right",
-      });
-
-      reset();
-      setEmailSubmitted(true);
-      setLoading(false);
-      return;
-    }
-
-    toast.error(
-      "Ocorreu um erro ao enviar a mensagem. Peço que tente novamente ou entre em contato através dos outros canais.",
-      {
-        position: "bottom-right",
-      }
-    );
-    setLoading(false);
-  };
 
   return (
     <section
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,27 +1,12 @@
 import type { Config } from "tailwindcss";
-const colors = require("tailwindcss/colors");
 
 const config: Config = {
-	content: [
-		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-	],
-	theme: {
-		extend: {
-			backgroundImage: {
-				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-				"gradient-conic":
-					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-			},
-			colors: {
-				...colors,
-				primary: colors.purple,
-				secondary: colors.pink,
-				grayText: "#ADB7BE",
-			},
-		},
-	},
-	plugins: [],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {},
+  plugins: [],
 };
 export default config;
